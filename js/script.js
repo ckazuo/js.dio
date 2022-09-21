e82fc4c015073b033bbf73faddc2e0c3bfd0e738
Cesar Kazuo Miya <ckazuo@gmail.com>
2022-09-20 22:24:27 -0300

    definir direcoes e impedir que a cobra suma

--- js/script.js
+++ js/script.js
@@ -23,7 +23,23 @@ function criarCobra() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "up")    direction = "down";
+    if (event.keyCode == 39 && direction != "left")  direction = "right";
+    if (event.keyCode == 40 && direction != "down")  direction = "up";
+}
+
 function iniciarJogo() {
+
+    /* cobra aparecer do outro lado */
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0        && direction == "left")  snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "up")    snake[0].y = 0;
+    if (snake[0].y < 0        && direction == "down")  snake[0].y = 16 * box;
+
     criarBG();
     criarCobra();
 
